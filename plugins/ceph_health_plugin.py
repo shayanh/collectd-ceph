c90abbf2750f52010831b1d406b24ae33e48aa99
Shayan Hosseini <shayan.hosseiny@gmail.com>
2017-11-15 12:26:34 +0330

    strip output

--- plugins/ceph_health_plugin.py
+++ plugins/ceph_health_plugin.py
@@ -27,7 +27,8 @@ class CephHealthPlugin(base.Base):
                            % (exc, traceback.format_exc()))
             return
 
-        if output.strip() is None:
+        output = output.strip()
+        if output is None:
             collectd.error('ceph-health: failed to ceph health :: output was None')
 
         if output == 'HEALTH_OK':
